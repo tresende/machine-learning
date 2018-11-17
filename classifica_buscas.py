e3dd73ee9387d9ea1d05654348f9b21a02339581
Thiago Resende <thiago.gcresende@gmail.com>
2018-11-17 15:11:43 -0200

    prevendo dados

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
 
 df = pd.read_csv('buscas.csv')
 
@@ -11,4 +12,31 @@ Ydummies = y_df
 x = Xdummies.values;
 y = Ydummies.values;
 
-print(y)
+portcentagem_de_treino = 0.9
+tamanho_de_treino = int(portcentagem_de_treino * len(y))
+tamanho_de_teste = int(len(y) - tamanho_de_treino)
+
+treino_dados = x[:tamanho_de_treino]
+treino_marcacoes = y[:tamanho_de_treino]
+
+teste_dados = x[-tamanho_de_teste:]
+teste_marcacoes = y[-tamanho_de_teste:]
+
+modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+print resultado
+
+diferencas = resultado - teste_marcacoes
+
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+# quantas vezes eu acertei ?
+print taxa_de_acerto
+
+# print(y)
