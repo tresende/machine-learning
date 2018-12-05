44319ac669a15e155332d66d473355e8cc26c944
Thiago Resende <thiago.gcresende@gmail.com>
2018-12-05 21:49:32 -0200

    atualizações

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,48 +1,84 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import pandas as pd
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
 from collections import Counter
 
+# teste inicial: home, busca, logado => comprou
+# home, busca
+# home, logado
+# busca, logado
+# busca: 85,71% (7 testes)
+
+porcentagem_treino = 0.8
+porcentagem_teste = 0.1
+
+
+def teste_real(modelo, validacao_dados, validacao_marcacoes):
+    resultado = modelo.predict(validacao_dados)
+    acertos = resultado == validacao_marcacoes
+    total_de_acertos = sum(acertos)
+    total_de_elementos = len(validacao_marcacoes)
+    taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+    msg = "Taxa de acerto do vencedor entre os dois algoritmos no mundo real: {0}".format(
+        taxa_de_acerto)
+    print(msg)
+
+
+def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes):
+    modelo.fit(treino_dados, treino_marcacoes)
+    resultado = modelo.predict(teste_dados)
+    acertos = resultado == teste_marcacoes
+    total_de_acertos = sum(acertos)
+    total_de_elementos = len(teste_dados)
+    taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+    msg = "Taxa de acerto do algoritmo {0}: {1}".format(nome, taxa_de_acerto)
+    print(msg)
+    return taxa_de_acerto
+
+
 df = pd.read_csv('buscas.csv')
+X_df = df[['home', 'busca', 'logado']]
+Y_df = df['comprou']
 
-x_df = df[['home', 'busca', 'logado']]
-y_df = df['comprou']
+Xdummies_df = pd.get_dummies(X_df)
+Ydummies_df = Y_df
 
-Xdummies = pd.get_dummies(x_df)
-Ydummies = y_df
+X = Xdummies_df.values
+Y = Ydummies_df.values
 
-x = Xdummies.values
-y = Ydummies.values
 
-portcentagem_de_treino = 0.9
-tamanho_de_treino = int(portcentagem_de_treino * len(y))
-tamanho_de_teste = int(len(y) - tamanho_de_treino)
+tamanho_de_treino = int(porcentagem_treino * len(Y))
+tamanho_de_teste = int(porcentagem_teste * len(Y))
+tamanho_de_validacao = len(Y) - tamanho_de_teste - tamanho_de_treino
 
-treino_dados = x[:tamanho_de_treino]
-treino_marcacoes = y[:tamanho_de_treino]
+treino_dados = X[0:tamanho_de_treino]
+treino_marcacoes = Y[0:tamanho_de_treino]
 
-teste_dados = x[-tamanho_de_teste:]
-teste_marcacoes = y[-tamanho_de_teste:]
+fim_do_teste = tamanho_de_treino + tamanho_de_teste
+teste_dados = X[tamanho_de_treino:fim_do_teste]
+teste_marcacoes = Y[tamanho_de_treino:fim_do_teste]
 
-modelo = MultinomialNB()
-modelo.fit(treino_dados, treino_marcacoes)
+validacao_dados = X[fim_do_teste:]
+validacao_marcacoes = Y[fim_do_teste:]
 
-resultado = modelo.predict(teste_dados)
+modeloMultinomial = MultinomialNB()
+resultadoMultinomial = fit_and_predict(
+    "MultinomialNB", modeloMultinomial, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
 
-diferencas = sum(resultado == teste_marcacoes)
-acertos = resultado == teste_marcacoes
+modeloAdaBoost = AdaBoostClassifier()
+resultadoAdaBoost = fit_and_predict(
+    "AdaBoostClassifier", modeloAdaBoost, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
 
-total_de_acertos = sum(acertos)
-total_de_elementos = len(teste_dados)
+if resultadoMultinomial > resultadoAdaBoost:
+    vencedor = modeloMultinomial
+else:
+    vencedor = modeloAdaBoost
 
-taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+teste_real(vencedor, validacao_dados, validacao_marcacoes)
 
-# eficacia do algoritimo que responde so um valor 0 ou 1, depende que qual aparecer mais no csv
+acerto_base = max(Counter(validacao_marcacoes).itervalues())
+taxa_de_acerto_base = 100.0 * acerto_base / len(validacao_marcacoes)
+print("Taxa de acerto base: %f" % taxa_de_acerto_base)
 
-taxa_de_acerto_base = 100.0 \
-    * max(Counter(teste_marcacoes).itervalues()) / len(teste_marcacoes)
-print 'Taxa de acerto base: %f' % taxa_de_acerto_base
-print 'Taxa de acerto do algoritmo: %f' % taxa_de_acerto
-
-			
+total_de_elementos = len(validacao_dados)
+print("Total de teste: %d" % total_de_elementos)
